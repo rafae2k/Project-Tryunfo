aa6cbce89e395728f589613f71503e0c02d1e856
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-01-30 13:54:05 -0300

    5. Faça a validação do botão de Salvar no formulário

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
       cardImage: '',
-      cardRare: '',
+      cardRare: 'Normal',
       cardTrunfo: '',
       hasTrunfo: false,
       isSaveButtonDisabled: true,
@@ -28,17 +28,44 @@ class App extends React.Component {
   }
 
   onInputChange({ target }) {
-    console.log(typeof target.value);
     const { name } = target;
     const value = (target.type === 'checkbox') ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.formValidation);
   }
 
   onSaveButtonClick(event) {
     event.preventDefault();
     console.log(event);
+  }
+
+  formValidation() {
+    const inputIsValid = this.inputValidation();
+    const attrIsValid = this.atributesValidation();
+    if (inputIsValid && attrIsValid) this.setState({ isSaveButtonDisabled: false });
+    else this.setState({ isSaveButtonDisabled: true });
+  }
+
+  inputValidation() {
+    const { cardName, cardDescription, cardImage, cardRare } = this.state;
+    const arr = [cardName, cardDescription, cardImage, cardRare];
+    return arr.map((value) => value.trim()).every((value) => value.length);
+  }
+
+  atributesValidation() {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+
+    const maxSumAttr = 210;
+    const maxInput = 90;
+
+    const attrInput = [+cardAttr1, +cardAttr2, +cardAttr3];
+    const sumAttr = attrInput.reduce((a, b) => a + b) <= maxSumAttr;
+    const attrRange = attrInput.every((value) => value >= 0 && value <= maxInput);
+
+    const isValid = sumAttr && attrRange;
+
+    return isValid;
   }
 
   render() {
