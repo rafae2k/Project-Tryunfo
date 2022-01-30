31314d9f15ed8ee9ebc5e97b4ad1fb96a23b2238
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-01-30 16:24:40 -0300

    create initial state file for app component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from './components/Card';
 import Form from './components/Form';
 import './App.css';
+import initialState from './initialState';
 
 class App extends React.Component {
   constructor() {
@@ -9,18 +10,7 @@ class App extends React.Component {
 
     this.onInputChange = this.onInputChange.bind(this);
 
-    this.state = {
-      cardName: '',
-      cardDescription: '',
-      cardAttr1: 0,
-      cardAttr2: 0,
-      cardAttr3: 0,
-      cardImage: '',
-      cardRare: 'Normal',
-      cardTrunfo: '',
-      hasTrunfo: false,
-      isSaveButtonDisabled: true,
-    };
+    this.state = { ...initialState };
   }
 
   componentDidMount() {
@@ -69,46 +59,16 @@ class App extends React.Component {
   }
 
   render() {
-    const {
-      cardName,
-      cardDescription,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardImage,
-      cardRare,
-      cardTrunfo,
-      hasTrunfo,
-      isSaveButtonDisabled,
-    } = this.state;
-
     return (
       <div className="card-builder">
         <Form
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardTrunfo={ cardTrunfo }
-          hasTrunfo={ hasTrunfo }
-          isSaveButtonDisabled={ isSaveButtonDisabled }
+          { ...this.state }
           onInputChange={ this.onInputChange }
           onSaveButtonClick={ this.onSaveButtonClick }
         />
 
-        <Card
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardTrunfo={ cardTrunfo }
-        />
+        <Card { ...this.state } />
+
       </div>
     );
   }
---
+++ src/initialState.js
@@ -0,0 +1,14 @@
+const initialState = {
+  cardName: '',
+  cardDescription: '',
+  cardAttr1: 0,
+  cardAttr2: 0,
+  cardAttr3: 0,
+  cardImage: '',
+  cardRare: 'Normal',
+  cardTrunfo: '',
+  hasTrunfo: false,
+  isSaveButtonDisabled: true,
+};
+
+export default initialState;
